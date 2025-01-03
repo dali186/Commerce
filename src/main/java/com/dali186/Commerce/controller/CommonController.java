f941834b92c2fa4398280d68adbce279c561a590
jwkim <wer07032@naver.com>
2025-01-03 16:14:58 +0900

    git push develop branch for test github Actions

---
+++ src/main/java/com/dali186/Commerce/controller/CommonController.java
@@ -0,0 +1,15 @@
+package com.dali186.Commerce.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/common")
+public class CommonController {
+
+	@RequestMapping("/v1")
+	public String githubActions() {
+		
+		return "success";
+	}
+}
